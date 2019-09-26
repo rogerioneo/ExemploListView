b8921f6c1a264a864c89aa5b01e3967fa94be8d2
rogerioneo <rogerio.neo@gmail.com>
2019-09-26 20:02:27 -0300

    Lista populada

--- app/src/main/java/br/edu/ifsp/exemplolistview/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/exemplolistview/MainActivity.kt
@@ -2,6 +2,8 @@ package br.edu.ifsp.exemplolistview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val listaItens = mutableListOf<String>()
@@ -10,10 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Criando uma lista
+        // Criando uma lista para usar como data
         for(i in 1..50) {
             listaItens.add("Item: ${i}")
         }
+
+        //Instanciando um ArrayAdapter
+        val adapter: ArrayAdapter<String> = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1,
+            listaItens)
+
+        itensLv.adapter = adapter
 
     }
 }
